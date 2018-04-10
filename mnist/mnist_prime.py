32732b77d407c934de2688bdd121fa4c6c79b613
tantexian <tantexian@meituan.com>
2018-04-10 11:45:47 +0800

    增加mnist简单用例

--- mnist/mnist_prime.py
+++ mnist/mnist_prime.py
@@ -1,3 +1,23 @@
+"""Functions for downloading and reading MNIST data. START"""
+""" 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# pylint: disable=unused-import
+import gzip
+import os
+import tempfile
+
+import numpy
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+# pylint: enable=unused-import
+"""
+"""Functions for downloading and reading MNIST data. END"""
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -7,15 +27,15 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 x = tf.placeholder(tf.float32, [None, 784])
 
 # 784维的图像像素值，10维的输出证据值向量
-w = tf.variable(tf.zeros([784, 10]))
+w = tf.Variable(tf.zeros([784, 10]))
 
-b = tf.variable(tf.zeros([10]))
+b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, w) + b)
 
-# 计算交叉熵 H = -Ey*log(y)
+# 计算交叉熵 H = -E(pk*log(1/qk)) (其中pk为真实分布，qk为非真实分布)
 y_ = tf.placeholder("float", [None, 10])
-
+# y是我们预测的概率分布, y_是实际的分布（我们输入的one-hot vector)
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 
 # 采用梯度下降法，0.01的学习速率 来训练，用梯度下降算法训练你的模型，微调你的变量，不断减少成本。
@@ -34,9 +54,10 @@ for i in range(1000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # tf.arg_max用于返回y数组中其中数值最大的值所在的索引
-correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
-print("correct_prediction: " + correct_prediction)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+print("correct_prediction: %s" % correct_prediction)
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, float))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+result = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+print("训练后模型的准确率为：%s" % result)
